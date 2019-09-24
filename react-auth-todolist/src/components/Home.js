e761996a657d5ec64304f9ea0a43cb858759caf7
Mikhail <mikhail97ru@rambler.ru>
2019-09-25 01:15:44 +0800

    added the Form and Table components into Home

--- react-auth-todolist/src/components/Home.js
+++ react-auth-todolist/src/components/Home.js
@@ -1,13 +1,35 @@
 // Home.js
 
 import React, { Component } from 'react';
+import Table from './Table'
+import Form from './Form'
 
 export default class Home extends Component {
+  state = {
+    characters: [
+    ],
+  }
+
+  removeCharacter = index => {
+    const { characters } = this.state
+
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index
+      }),
+    })
+  }
+
+  insertCharacter = character => {
+    this.setState({ characters: [...this.state.characters, character] })
+  }
+
   render() {
     return (
-      <div>
-        Home Component
-            </div>
+      <div className="container">
+        <Table characterData={characters} removeCharacter={this.removeCharacter} />
+        <Form handleSubmit={this.insertChracter} />
+      </div>
     )
   }
 }
